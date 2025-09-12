d92fbb0d7725d328008d85213ef9f2e592e44b6c
Nodar Gogoberidze <gnodar01@gmail.com>
2025-09-12 16:41:36 -0400

    Get reader from proper data path

--- src/daskrw/tozarr.py
+++ src/daskrw/tozarr.py
@@ -1,9 +1,12 @@
 import sys
+import os
+import pathlib
 from .reader import TiledImageReader
 
 def main() -> int:
-    reader = TiledImageReader('./data/tiledimg.ome.tiff')
-    print(reader.file_path)
+    reader = TiledImageReader(pathlib.Path(os.path.dirname(__file__)) / 'data/tiledimg.ome.tiff')
+    img = reader.read_tiled()
+
     return 0
 
 if __name__ == "__main__":
